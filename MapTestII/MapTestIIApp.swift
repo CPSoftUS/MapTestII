a28e29c3eef6347470f25b41f1ea5fcf8cabbcc8
CPSoft.us <danny.uff@gmail.com>
2024-08-17 12:31:29 -0400

    Initial Commit

---
+++ MapTestII/MapTestIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestIIApp.swift
+//  MapTestII
+//
+//  Created by Dan Uff on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MapTestIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapTestIITests/MapTestIITests.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestIITests.swift
+//  MapTestIITests
+//
+//  Created by Dan Uff on 8/17/24.
+//
+
+import Testing
+@testable import MapTestII
+
+struct MapTestIITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
